55678b09df941c5a720ecd6591731a13f871f3e5
marryamcisco12-gif <marryamcisco12@gmail.com>
2026-02-13 01:13:54 +0500

    Update lstm_model.py

--- models/lstm_model.py
+++ models/lstm_model.py
@@ -1,1 +1,22 @@
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+
+def build_lstm(input_shape):
+
+    model = Sequential()
+
+    model.add(LSTM(64, return_sequences=True,
+                   input_shape=input_shape))
+    model.add(Dropout(0.2))
+
+    model.add(LSTM(32))
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(
+        optimizer='adam',
+        loss='binary_crossentropy',
+        metrics=['accuracy']
+    )
+
+    return model
